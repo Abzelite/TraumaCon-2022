09934c9b75496bee0f8ff9c568f2c4a6051d17b6
Abzelite <109804681+Abzelite@users.noreply.github.com>
2022-12-18 16:31:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function comittee()
+{
+    window.location="comittee.html";
+}
+function show()
+{
+   window.location="index1.html";
+}
+function show1()
+{
+   window.location="index2.html";
+}
+function one()
+{
+window.location="day1.html";
+}
+function two()
+{
+    window.location="day2.html";
+}
+var preloader=  document.getElementById('loading');
+function myfunction()
+{
+ preloader.style.display='none';
+}
+function sign()
+{
+    window.location="sign.html";
+}
+function faculty()
+{
+    window.location="faculty.html";
+}
